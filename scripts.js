7a7670620e0768966fef2eadf30e72a130efe3a8
isabelmojicapardo-sketch <isabelmojicapardo@gmail.com>
2026-03-09 10:01:33 -0500

    Update click event for varTitulo with state toggle
    Fix typo in backgroundColor property and update text based on estadoInicial.

--- scripts.js
+++ scripts.js
@@ -3,7 +3,15 @@
 const varTitulo = document.getElementById("titulo");
 
 varTitulo.addEventListener("click",() => {
+  if(estadoInicial=== true) {
   varTitulo.innerText = "¿¡Buenos días!";
   varTitulo.style.color = "#ff0080";
   varTitulo.style.backgroudColor = "white";
-  
+} else {
+ varTitulo.innerText = "Hola";
+ varTitulo.style.color = "#572364";
+ varTitulo.style.backgroudColor = "transparent";
+ }
+ estadoInicial = !estadoInicial;
+})
+    
