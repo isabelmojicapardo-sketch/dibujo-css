b15a9d3d8004845d5e78ae5c15eec901d17dbf3a
isabelmojicapardo-sketch <isabelmojicapardo@gmail.com>
2026-03-09 09:31:40 -0500

    Implement click event on titulo element
    Add click event listener to varTitulo to change text and style.

--- scripts.js
+++ scripts.js
@@ -2,4 +2,8 @@
 
 const varTitulo = document.getElementById("titulo");
 
-varTitulo
+varTitulo.addEventListener("click",() => {
+  varTitulo.innerText = "¿¡Buenos días!";
+  varTitulo.style.color = "#ff0080";
+  varTitulo.style.backgroudColor = "white";
+  
